b559072327cee63d10691e3915a60f4720056982
ringtail003 <ringtail003@gmail.com>
2019-10-20 17:43:49 +0900

    page componentをルーティングの1ページとする

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BudgetComponent } from './pages/budget/budget.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'budget',
+    component: BudgetComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
