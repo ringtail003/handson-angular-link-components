7ddec9deb940e35f29975a85ca265b8bdee1f259
ringtail003 <ringtail003@gmail.com>
2019-10-20 17:00:01 +0900

    unit component（リスト）追加

--- src/app/shared/components/units/module.ts
+++ src/app/shared/components/units/module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { AccountPickerComponent } from './account-picker/account-picker.component';
+import { AccountListComponent } from './account-list/account-list.component';
 
 @NgModule({
   declarations: [
     AccountPickerComponent,
+    AccountListComponent,
   ],
   exports: [
     AccountPickerComponent,
+    AccountListComponent,
   ],
 })
 export class SharedUnitComponentModule {}
